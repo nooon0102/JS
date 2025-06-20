c37ef287d5b2754943a3a6bb40466ed65bc0ea1f
nooon0102 <nozomi.imai12@gmail.com>
2025-06-20 23:30:41 +0900

    イベント発火 基礎編

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ window.addEventListener('load', function() {
   const pullDownButton = document.getElementById("lists");
 
   pullDownButton.addEventListener("mouseover", function() {
-    console.log("乗る");
+    pullDownButton.setAttribute("style" , "background-color:blue")
+    console.log("乗った時は青");
   });
 
   pullDownButton.addEventListener("mouseout", function() {
-    console.log("外れる");
+    pullDownButton.removeAttribute("style" , "background-color:red")
+    console.log("外れたら赤");
   });
 
   pullDownButton.addEventListener("click", function() {
-    console.log("クリック");
+    pullDownButton.setAttribute("style", "background-color:green");
+    console.log("クリックは緑");
   });
 });
